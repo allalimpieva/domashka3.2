ed77cc821b004dcfd65ecd3366507651006e56a1
Alla <allafaa@yandex.ru>
2023-01-19 00:03:44 +0300

    добавили задачу 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+//1 . программа, которая на вход принимает два числа и выдает max и min
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    int max = a;
+    int min = b;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.WriteLine(min);
+}
+else if (a < b)
+{
+    int max = b;
+    int min = a;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.WriteLine(min);
+}
+
